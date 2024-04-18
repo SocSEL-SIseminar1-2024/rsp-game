67f13d606bf079300f5186192e9db3d9ced905d1
momoka-h <141204768+momoka-h@users.noreply.github.com>
2024-04-18 11:35:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# じゃんけんゲーム ✊✌️✋（初級）
+
+## 実行方法
+
+1. 以下のURLにアクセス
+
+[![Open in Cloud Shell](https://gstatic.com/cloudssh/images/open-btn.svg)](https://ide.cloud.google.com/?cloudshell_git_repo=https://github.com/SocSEL-SIseminar1-2024/rsp-game&cloudshell_workspace=./&cloudshell_tutorial=README.md)
+
+2. Trust repoにチェックを入れて確認を押します．
+3. 上の項目から，Terminal > Run Build Taskを押すと，プログラムが実行されます．
+
+
+
+※Eclipse をインストール済みの方は Eclipse を利用してもらって大丈夫です．
+
+## 課題の例
+
+- 不具合の修正
+- リファクタリング
+- 機能追加（ゲームのコンティニューやあっち向いてホイ等）
+
+※機能追加やリファクタリングの過程でファイルを分けてもらっても構いません．
+
+ファイルを追加する際はRSP.javaと同じフォルダ内であれば，import文を書かずにそのままインスタンス作成してください．
+
+フォルダを分ける場合でコンパイルできないときは相談してください．
---
+++ RSP.java
@@ -0,0 +1,79 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RSP {
+	public static void main(String[] args) {
+		// テキストに使う色の宣言
+		final String GREEN = "\u001b[00;31m";
+		final String YELLOW = "\u001b[00;33m";
+		final String PURPLE = "\u001b[00;34m";
+		// プレイヤーの手を入力
+		System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
+		Scanner scan = new Scanner(System.in);
+		int player_hand = Integer.parseInt(scan.nextLine());
+		scan.close();
+
+		// 相手の手を決定
+		int enemy_hand = getRandomInt(2);
+		System.out.println(" Enemy hand is: " + enemy_hand);
+
+		System.out.print("Result: ");
+		// 勝敗判定
+		switch (player_hand) {
+			case 0:
+				switch (enemy_hand) {
+					case 0:
+						System.out.println(GREEN + "Draw!");
+						break;
+					case 1:
+						System.out.println(YELLOW + "You win!");
+						break;
+					case 2:
+						System.out.println(PURPLE + "Enemy win!");
+						break;
+					default:
+						break;
+				}
+				break;
+			case 1:
+				switch (enemy_hand) {
+					case 0:
+						System.out.println(GREEN + "Draw!");
+						break;
+					case 1:
+						System.out.println(YELLOW + "You win!");
+						break;
+					case 2:
+						System.out.println(PURPLE + "Enemy win!");
+						break;
+					default:
+						break;
+				}
+				break;
+			case 2:
+				switch (enemy_hand) {
+					case 0:
+						System.out.println(YELLOW + "You win!");
+						break;
+					case 1:
+						System.out.println(PURPLE + "Enemy win!");
+						break;
+					case 2:
+						System.out.println(GREEN + "Draw!");
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	// 受け取った範囲でランダムな数値を生成
+	private static int getRandomInt(int range) {
+		Random random = new Random();
+		int random_num = random.nextInt(range);
+		return random_num;
+	}
+}
