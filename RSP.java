4f177435b92d7ae89a2723bc48484db361507b6d
momota1013 <110619268+momota1013@users.noreply.github.com>
2024-04-23 16:15:35 +0900

    ゲームモードの選択の実装
    選ぶ数字でゲームモードが選べるようにした。（ゲームは実装中）

--- RSP.java
+++ RSP.java
@@ -7,67 +7,75 @@ public class RSP {
 		final String GREEN = "\u001b[00;31m";
 		final String YELLOW = "\u001b[00;33m";
 		final String PURPLE = "\u001b[00;34m";
-		// プレイヤーの手を入力
-		System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
-		Scanner scan = new Scanner(System.in);
-		int player_hand = Integer.parseInt(scan.nextLine());
-		scan.close();
+        System.out.print("Please choose game mode 0:noraml 1:otokogi");
+        Scanner scan = new Scanner(System.in);
+        int gamemode = Integer.parseInt(scan.nextLine());
+        //ゲームモードで別のゲームを選べる
+        if (gamemode == 0) {
+            // プレイヤーの手を入力
+            System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
+            int player_hand = Integer.parseInt(scan.nextLine());
+            scan.close();
 
-		// 相手の手を決定
-		int enemy_hand = getRandomInt(2);
-		System.out.println(" Enemy hand is: " + enemy_hand);
+            // 相手の手を決定
+            int enemy_hand = getRandomInt(2);
+            System.out.println(" Enemy hand is: " + enemy_hand);
 
-		System.out.print("Result: ");
-		// 勝敗判定
-		switch (player_hand) {
-			case 0:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(GREEN + "Draw!");
-						break;
-					case 1:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 2:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					default:
-						break;
-				}
-				break;
-			case 1:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(GREEN + "Draw!");
-						break;
-					case 1:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 2:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					default:
-						break;
-				}
-				break;
-			case 2:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 1:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					case 2:
-						System.out.println(GREEN + "Draw!");
-						break;
-					default:
-						break;
-				}
-				break;
-			default:
-				break;
-		}
+            System.out.print("Result: ");
+            // 勝敗判定
+            switch (player_hand) {
+                case 0:
+                    switch (enemy_hand) {
+                        case 0:
+                            System.out.println(GREEN + "Draw!");
+                            break;
+                        case 1:
+                            System.out.println(YELLOW + "You win!");
+                            break;
+                        case 2:
+                            System.out.println(PURPLE + "Enemy win!");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (enemy_hand) {
+                        case 0:
+                            System.out.println(GREEN + "Draw!");
+                            break;
+                        case 1:
+                            System.out.println(YELLOW + "You win!");
+                            break;
+                        case 2:
+                            System.out.println(PURPLE + "Enemy win!");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (enemy_hand) {
+                        case 0:
+                            System.out.println(YELLOW + "You win!");
+                            break;
+                        case 1:
+                            System.out.println(PURPLE + "Enemy win!");
+                            break;
+                        case 2:
+                            System.out.println(GREEN + "Draw!");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if(gamemode == 1){
+            System.out.println("実装中");
+        }
 	}
 
 	// 受け取った範囲でランダムな数値を生成
