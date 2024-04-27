fdff1db56818771f4e4c968bdd8a017f3e991982
YamaguchiSora08 <167664621+YamaguchiSora08@users.noreply.github.com>
2024-04-27 16:40:33 +0900

    じゃんけんほいほいに改良
    じゃんけんを行う際お互いに候補となる手を2つ選びそれを提示した後に最終的な手を決めてその結果でじゃんけんを行わせるように変更

--- RSP.java
+++ RSP.java
@@ -4,69 +4,53 @@ import java.util.Scanner;
 public class RSP {
 	public static void main(String[] args) {
 		// テキストに使う色の宣言
-		final String GREEN = "\u001b[00;31m";
+		final String GREEN = "\u001b[00;32m";
 		final String YELLOW = "\u001b[00;33m";
 		final String PURPLE = "\u001b[00;34m";
-		// プレイヤーの手を入力
-		System.out.print("Please input your hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
-		Scanner scan = new Scanner(System.in);
-		int player_hand = Integer.parseInt(scan.nextLine());
-		scan.close();
+		// プレイヤーの手の候補入力
 
-		// 相手の手を決定
-		int enemy_hand = getRandomInt(2);
-		System.out.println(" Enemy hand is: " + enemy_hand);
+		Scanner scan = new Scanner(System.in);
+		try{
+		System.out.print("Please input your candidate of the hand two! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
+		int player_hand_1 = Integer.parseInt(scan.nextLine());
+		System.out.print(" or ");
+		int player_hand_2 = Integer.parseInt(scan.nextLine());
+
+		// 相手の手の候補決定
+		int enemy_hand_1 = getRandomInt(2);
+		int enemy_hand_2 = getRandomInt(2);
+		System.out.println(" Enemy hand is: " + enemy_hand_1 + " or " +enemy_hand_2);
+
+		//自分の手の最終決定
+		System.out.print("Please input your final hand! 0:Rock 1:Scissors 2:Paper\n Your hand is : ");
+		int player_hand = Integer.parseInt(scan.nextLine());
+		if ((player_hand==player_hand_1) || (player_hand == player_hand_2)){
+			break;
+		} else {
+			System.out.println("Please inuput" + player_hand_1 +"or"+player_hand_2);
+		}
+		} catch (NumberFormatException e) {
+			System.out.println("Please input number");
+
+		//相手の手を最終決定
+		int random_choice = getRandomInt(1); // 0または1のランダムな値を取得
+            	int enemy_hand;
+            	if (random_choice == 0) {
+                	enemy_hand = enemy_hand_1;
+            	} else {
+                	enemy_hand = enemy_hand_2;
+            	}
+
+		scan.close();
 
 		System.out.print("Result: ");
 		// 勝敗判定
-		switch (player_hand) {
-			case 0:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(GREEN + "Draw!");
-						break;
-					case 1:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 2:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					default:
-						break;
-				}
-				break;
-			case 1:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(GREEN + "Draw!");
-						break;
-					case 1:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 2:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					default:
-						break;
-				}
-				break;
-			case 2:
-				switch (enemy_hand) {
-					case 0:
-						System.out.println(YELLOW + "You win!");
-						break;
-					case 1:
-						System.out.println(PURPLE + "Enemy win!");
-						break;
-					case 2:
-						System.out.println(GREEN + "Draw!");
-						break;
-					default:
-						break;
-				}
-				break;
-			default:
-				break;
+		if (player_hand == enemy_hand){
+			System.out.println(GREEN + "Draw!");
+		} else if((player_hand+1==enemy_hand) ||(player_hand-2==enemy_hand)){
+			System.out.println(YELLOW + "You win!");
+		} else{
+			System.out.println(PURPLE + "Enemy win!");
 		}
 	}
 
